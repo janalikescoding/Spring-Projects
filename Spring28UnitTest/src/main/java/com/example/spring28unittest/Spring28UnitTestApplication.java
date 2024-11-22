b35ceee6281f3e116e26668b9dd5335bdebf697e
Janarthana S Sivaraman <jana.sivaraman@entrust.com>
2024-11-21 20:21:50 -0500

    21Nov24

---
+++ Spring28UnitTest/src/main/java/com/example/spring28unittest/Spring28UnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring28unittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring28UnitTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Spring28UnitTestApplication.class, args);
+  }
+
+}
---
+++ Spring28UnitTest/src/test/java/com/example/spring28unittest/Spring28UnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring28unittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring28UnitTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
