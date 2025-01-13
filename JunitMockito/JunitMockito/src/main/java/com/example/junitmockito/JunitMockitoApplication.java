5ffe31d0f3b1134b640324261b0fac9144ce3db5
Janarthana S Sivaraman <jsivaraman@yahoo.com>
2025-01-12 19:05:18 -0500

    12-Jan-25: Test hot-swapping Java code

--- JunitMockito/JunitMockito/src/main/java/com/example/junitmockito/JunitMockitoApplication.java
+++ JunitMockito/JunitMockito/src/main/java/com/example/junitmockito/JunitMockitoApplication.java
@@ -1,13 +1,18 @@
 package com.example.junitmockito;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JunitMockitoApplication {
+public class JunitMockitoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(JunitMockitoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World!");
+    }
 }
